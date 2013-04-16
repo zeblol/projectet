82c8a5c9f18e92e67c9bc108fd559bcafb227bde
SebastianC <seb_chimera@hotmail.com>
2013-04-16 08:59:19 +0200

    test

---
+++ src/dbtest/DB.java
@@ -0,0 +1,15 @@
+package dbtest;
+
+
+/**
+ *
+ * @author Sebastian
+ */
+public class DB {
+    String Driver = "placeholder";
+    
+    String id = "placeholder";
+    
+    String pw = "placeholder";
+    
+}
